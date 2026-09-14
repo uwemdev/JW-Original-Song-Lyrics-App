03ec113d66962104afb17c45c3974fc90b01d300
uwemdev <uwemuwemetim@gmail.com>
2026-09-14 01:32:47 +0100

    Fix syntax error in NotificationsScreen

--- mobile/src/screens/NotificationsScreen.js
+++ mobile/src/screens/NotificationsScreen.js
@@ -237,8 +237,6 @@ const makeStyles = (colors) => StyleSheet.create({
   },
   backBtn: {
     width: 40,
-  backBtn: {
-    width: 40,
     height: 40,
     borderRadius: 20,
     backgroundColor: 'rgba(255,255,255,0.05)',
